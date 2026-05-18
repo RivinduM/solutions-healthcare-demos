e4b73602a9bad100f53a449e2026f4a29a77072c
Rivindu <rivindum.15@cse.mrt.ac.lk>
2026-05-18 14:11:19 +0530

    Update configs

--- apps/demo-diagnostic-report-viewer-smart-app/public/config.js
+++ apps/demo-diagnostic-report-viewer-smart-app/public/config.js
@@ -1,10 +1,13 @@
 window.Config = {
   clientId: "",
+  clientSecret: "",
   redirectUri: "http://localhost:3000",
-  scope: "launch openid patient/DiagnosticReport.r",
+  scope: "launch openid fhirUser patient/DiagnosticReport.s patient/Observation.s",
   // Used as fallback if SMART discovery (.well-known/smart-configuration) fails
   // authorizationEndpoint: "https://api.asgardeo.io/t/wso2ob/oauth2/authorize",
-  authorizationEndpoint: "https://localhost:9443/oauth2/authorize",
-  tokenEndpoint: "https://localhost:9443/oauth2/token",
-  fhirBaseUrl: "http://localhost:9090/fhir/r4",
+  authorizationEndpoint: "https://localhost:9453/oauth2/authorize",
+  tokenEndpoint: "https://localhost:9453/oauth2/token",
+  fhirBaseUrl: "https://localhost:8243/r4",
+  diagnosticReportUrl: "/DiagnosticReport",
+  observationUrl: "/Observation",
 };
